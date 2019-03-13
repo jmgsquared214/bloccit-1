efe7cf53f115df7cc3eae6755bd436a17c01eb50
Jared Davies <jdaviesgolf@pga.com>
2019-03-13 14:27:25 -0400

    Intro to Express Assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,20 +4,25 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
 
-//#2
     it("should return status code 200", (done) => {
-
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
         done();
       });
     });
-
   });
+  
+  describe("GET /marco", () => {
+
+    it("should return status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+
 });

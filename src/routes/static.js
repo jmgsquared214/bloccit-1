68fef2998bb62f1a8c510deeec9d481078f7e85f
Jamie Michelle Gammel <jmgsquared214@gmail.com>
2019-03-14 08:59:32 -0700

    changing structure adding html(ejs) index file

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
-
-module.exports = router;
+// const express = require("express");
+// const router = express.Router();
+//
+// router.get("/", (req, res, next) => {
+//   res.send("Welcome to Bloccit");
+// });
+//
+// module.exports = router;

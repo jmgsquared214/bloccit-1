050bb33d63376441685f641dff9f5954026b4e6b
Jared Davies <jdaviesgolf@pga.com>
2019-03-14 00:12:16 -0400

    Back to Checkpoint Original

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const http = require("http");
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-console.log (port);
-
 const server = http.createServer(app);
 
 server.listen(port);

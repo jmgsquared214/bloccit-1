1624a933cff83d61450da7b1f8a4aec2181a7ecc
Jared Davies <jdaviesgolf@pga.com>
2019-03-13 22:35:29 -0400

    Bug Fix 7

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const http = require("http");
 
-const port = process.env.PORT||3000;
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const server = http.createServer(app);
@@ -9,16 +9,16 @@ const server = http.createServer(app);
 server.listen(port);
 
 function normalizePort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
   }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
-    console.log(`server is listening for requests on port ${server.address().port}`);
+  console.log(`server is listening for requests on port ${server.address().port}`);
 });
